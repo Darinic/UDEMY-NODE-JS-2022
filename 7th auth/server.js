9c5b96008be25b320d5032fb9bd2c529d3c47f2a
Darinic <97019341+Darinic@users.noreply.github.com>
2022-06-24 17:26:03 +0300

    security

---
+++ 7th auth/server.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const fs = require("fs");
+const helmet = require("helmet");
+const https = require("https");
+const path = require("path");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+
+require("dotenv").config();
+const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback)); //important
+
+const app = express();
+
+app.use(helmet());
+app.use(passport.initialize());
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; //TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+  next();
+}
+
+app.get('/auth/google', 
+  passport.authenticate('google', {
+    scope: ['email']
+  }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),(req, res) => {
+    console.log('Google called us back!');
+  }
+);
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in!");
+});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+  return res.send("Your perrsonal secret value is 42!");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      cert: fs.readFileSync("cert.pem"),
+      key: fs.readFileSync("key.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
